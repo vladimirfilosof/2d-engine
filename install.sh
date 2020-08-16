3726e96f5389cc9a887780284052dc1b1ab6e71e
vrodedanya <socurahlem@yandex.ru>
2020-08-16 21:35:31 +0300

    Fixed bugs in install.sh
    Now for copy library to /usr/local/include used rsync

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 sudo mkdir /usr/local/include/Engine
 make -f build/makefile static_library
-sudo mv libengine.a /usr/local/lib/
-sudo cp -r source/ /usr/local/include/Engine
+sudo mv libengine.a /usr/local/lib/ 
+sudo rsync --exclude="*.cpp" -r --progress source/ /usr/local/include/Engine
 echo Library installed!
