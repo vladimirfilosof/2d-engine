879484b28ba15f2a34ba2277ce362a3cc6719e2c
vrodedanya <socurahlem@yandex.ru>
2020-10-24 23:34:52 +0300

    Fixed misstype

--- source/ECS/Systems/SoundSystem.cpp
+++ source/ECS/Systems/SoundSystem.cpp
@@ -7,7 +7,7 @@
 
 SoundSystem::SoundSystem(int frequency, Uint16 format, int channels, int chunksize)
 {
-	if (Mix_OpenAudio(frequency, format, channels, chunksize < 0))
+	if (Mix_OpenAudio(frequency, format, channels, chunksize) < 0)
 		throw std::runtime_error("[SoundSystem] <SoundSystem>: Open sound mixer error");
 }
 
