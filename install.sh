d1258a4f4a64d62abd0ac78a4fc662e557e59e16
vrodedanya <socurahlem@yandex.ru>
2020-11-19 00:52:00 +0300

    Fixed bug in install.sh
    Before this commit libengine.a was empty after building
    and this was a cause of linker error in other projects

--- install.sh
+++ install.sh
@@ -4,9 +4,11 @@ rm -r /usr/local/include/Engine
 rm -r /usr/local/lib/libengine.a
 
 mkdir /usr/local/include/Engine
-make -f build/makefile static_library
-chown root libengine.a
-chmod +rwx libengine.a
+cd build
+make static_library
+chown root ./libengine.a
+chmod +rwx ./libengine.a
 mv libengine.a /usr/local/lib/ 
+cd ..
 rsync --exclude="*.cpp" -r source/ /usr/local/include/Engine
 echo Library installed!
